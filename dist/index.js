c1d9dc5430ccb240cddd9ee380858b4e305c8f82
DimaGrimach <chiki.briki2@gmail.com>
2023-07-03 18:08:46 +0300

    Added CRUD operations as Pest Ati works.

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const parserMiddleWare = (0, body_parser_1.default)({});
 const exprApp = (0, express_1.default)();
+exprApp.use(parserMiddleWare);
 const port = 3000;
 const products = [{ title: 'tomato', id: 1 }, { title: 'banana', id: 2 }];
 exprApp.get('/productsI/:ID', (req, res) => {
@@ -18,15 +21,62 @@ exprApp.get('/products', (req, res) => {
     const reqQueryParam = req.query;
     const arrQueryParams = Object.keys(reqQueryParam);
     let result;
-    arrQueryParams.forEach(function (oneParam, i) {
+    arrQueryParams.forEach(function (oneParam) {
         if (products[0][oneParam]) {
             result = products.find(function (product) {
                 return product[oneParam].includes(reqQueryParam[oneParam]);
             });
         }
     });
-    console.log('!-', result);
     result ? res.send(result) : res.sendStatus(404);
+});
+exprApp.get('/productsAll', (req, res) => {
+    products ? res.send(products) : res.sendStatus(404);
+});
+exprApp.delete('/products/:ID', (req, res) => {
+    const uriProdID = req.params.ID;
+    let findedProduct;
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +uriProdID) {
+            findedProduct = products[i];
+            products.splice(i, 1);
+            break;
+        }
+    }
+    findedProduct ? res.sendStatus(204) : res.sendStatus(404);
+});
+exprApp.post('/products', (req, res) => {
+    const reqBody = req.body.title;
+    let newId = Math.round(Math.random() * 10) + 3;
+    const containsThisId = products.some(function (product) {
+        return product.id === newId;
+    });
+    let temp;
+    if (containsThisId)
+        temp = Math.round(Math.random() * (200 + newId)) + newId;
+    newId = containsThisId ? temp : newId;
+    if (reqBody) {
+        let newProd;
+        newProd = {
+            title: reqBody,
+            id: newId
+        };
+        products.push(newProd);
+        res.status(201).send(newProd);
+    }
+    else
+        res.sendStatus(404);
+});
+exprApp.put('/products/:ID', (req, res) => {
+    const iDFromReqParams = req.params.ID;
+    const titleInBody = req.body.title;
+    const foundProduct = products.find(function (product) {
+        return product.id === +iDFromReqParams;
+    });
+    if (foundProduct) {
+        foundProduct.title = titleInBody;
+    }
+    foundProduct ? res.status(204).send(foundProduct) : res.sendStatus(404);
 });
 exprApp.listen(port, () => {
     console.log(`Example MYapp listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
+const parserMiddleWare = bodyParser({})
 const exprApp = express()
-const port = 3000
+exprApp.use(parserMiddleWare);
+const port: number = 3000
 
 interface IProduct {
     title: string
@@ -22,16 +25,66 @@ exprApp.get('/products', (req: Request, res: Response) => {
     const arrQueryParams: string[] = Object.keys(reqQueryParam);
     let result: IProduct | undefined;
 
-    arrQueryParams.forEach(function (oneParam,i){
-        if(products[0][oneParam]) {
+    arrQueryParams.forEach(function (oneParam) {
+        if (products[0][oneParam]) {
             result = products.find(function (product) {
-                return  product[oneParam].includes(reqQueryParam[oneParam]);
+                return product[oneParam].includes(reqQueryParam[oneParam]);
             });
         }
     });
     result ? res.send(result) : res.sendStatus(404)
+});
+
+exprApp.get('/productsAll', (req: Request, res: Response) => {
+    products ? res.send(products) : res.sendStatus(404)
 })
 
+exprApp.delete('/products/:ID', (req: Request, res: Response) => {
+    const uriProdID: string = req.params.ID;
+    let findedProduct: IProduct | undefined;
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +uriProdID) {
+            findedProduct = products[i];
+            products.splice(i, 1);
+            break;
+        }
+    }
+    findedProduct ? res.sendStatus(204) : res.sendStatus(404)
+})
+
+exprApp.post('/products', (req: Request, res: Response) => {
+    const reqBody: string = req.body.title;
+    let newId: number = Math.round(Math.random() * 10) + 3;
+    const containsThisId: boolean = products.some(function (product) {
+        return product.id === newId;
+    });
+    let temp:number;
+    if (containsThisId) temp = Math.round(Math.random() * (200 + newId)) + newId
+    newId = containsThisId ? temp : newId;
+
+    if (reqBody) {
+        let newProd: IProduct;
+        newProd = {
+            title: reqBody,
+            id: newId
+        }
+        products.push(newProd);
+        res.status(201).send(newProd);
+    } else res.sendStatus(404)
+});
+
+exprApp.put('/products/:ID', (req: Request, res: Response) => {
+    const iDFromReqParams: string = req.params.ID;
+    const titleInBody: string = req.body.title;
+    const foundProduct: IProduct | undefined = products.find(function (product) {
+        return product.id === +iDFromReqParams;
+    });
+    if(foundProduct){
+        foundProduct.title=titleInBody;
+    }
+    foundProduct ? res.status(204).send(foundProduct) : res.sendStatus(404)
+})
 
 exprApp.listen(port, () => {
     console.log(`Example MYapp listening on port ${port}`)
